799c0df4b5e6b211cd3ae4aadbd90c785abb56cb
htorres5 <99515061+htorres5@users.noreply.github.com>
2023-05-01 16:37:01 -0700

    changed combobreak sound and explanatory comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,37 @@
+/* 
+
+* Hector Torres
+* Rocket Patrol: Apollo's Rebirth
+* 10 hours
+
+* Mods List:
+
+* - Track a high score that persists across scenes and display it in the UI (5)
+* - Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
+* - Implement the speed increase that happens after 30 seconds in the original game (5)
+    ! NOTE: The game speeds up 20% every 10 seconds (until 300%)
+* - Randomize each spaceship's movement direction at the start of each play (5)
+* Allow the player to control the Rocket after it's fired (5)
+
+* - Display the time remaining (in seconds) on the screen (10)
+* - Create a new title screen (e.g., new artwork, typography, layout) (10)
+* - Implement parallax scrolling for the background (10)
+
+* - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+* Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
+* Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (15)
+
+* Sources:
+
+*BGM: https://peritune.com/blog/2021/09/08/retrorpg_battle2/
+*Planets in BG: https://helianthus-games.itch.io/pixel-art-planets?download
+*Font: https://fonts2u.com/pixel-nes.font
+*SFX: https://freesound.org/people/LittleRobotSoundFactory/packs/16681/
+*https://pixabay.com/sound-effects/search/8-bit/
+
+* WIll add the ability to save highest combo to local storage like high score (after its due tho)
+
+*/
 console.log("uwu")
 
 let config = {
